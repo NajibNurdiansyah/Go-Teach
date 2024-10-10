74c1a218c02ef33a5a1eeb4d1fc17dea2f2e374a
qhdrahmd <ahmdnew7@gmail.com>
2024-10-10 22:53:47 +0800

    js landing pagenya bang achmad

--- landing-page.js
+++ landing-page.js
@@ -1,3 +1,16 @@
+// Fungsi untuk menampilkan pop-up ketika tombol diklik
+document.addEventListener('DOMContentLoaded', function() {
+    // Ambil tombol berdasarkan ID
+    const choosePlanButton = document.getElementById('pricing');
+    
+    // Tambahkan event listener untuk klik pada tombol
+    choosePlanButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Mencegah link untuk melakukan navigasi
+        alert("You have selected the Subscription!");
+    });
+});
+
+
 // Teks testimonial yang ingin ditampilkan dengan efek pengetikan
 const testimonialText = "Aplikasi Go Teach sangat membantu saya menemukan tutor yang sesuai dengan kebutuhan saya. Saya bisa belajar dengan fleksibel tanpa harus terikat waktu. Sejauh ini saya sangat menyukai Go Teach.";
 const typingSpeed = 50; // Kecepatan pengetikan dalam milidetik
